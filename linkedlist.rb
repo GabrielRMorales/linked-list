be09760c5dcb1c1abdb41a3c3cbb688a5802d669
Gabriel Morales <grmorale@gmail.com>
2017-04-18 21:01:12 -0700

    Define basic linked list methods

--- linkedlist.rb
+++ linkedlist.rb
@@ -8,75 +8,112 @@ end
 
 def prepend(data)
   if @head.nil?
-    #means its empty
     @head=Node.new(data)
-    @tail=data
-    @tail=Node.new(data, nil)
+    @tail=@head
   else
-    #set new node's next to the current head
-    temp=@head.value
+    temp=@head
     @head=Node.new(data, temp)
   end
 end
 
 def append(data)
-  if @tail.nil?
-    #means tails is empty
-    @tail=data
-    @head=data
+  if @head.nil?
+    @head=Node.new(data)
+    @tail=@head 
   else
-    #set the next node of the tail to nil
-    data.next_node(nil)
-    @tail=data
+    if @head.next_node.nil?
+      @tail=Node.new(data)
+      @head.next_node=@tail
+    else 
+      temp=@tail
+      @tail=Node.new(data)
+      temp.next_node=@tail
+    end
   end
 end
 
 def head
-@head.value
+@head.data
 end
 
 def tail
-@tail.value
+@tail.data
 end
 
 def size
   temp=@head
-  counter=0
-  while (temp!=nil)
+  counter=1
+  while (temp.next_node!=nil)
+    temp=temp.next_node
     counter+=1
-    temp=temp.next
-    
   end
   counter
 end
 
+def at(index)
+  i=0
+  temp=@head
+  while (i!=index && temp!=nil)
+    temp=temp.next_node
+    i+=1
+  end
+  return temp.data
 end
 
+def pop
+ temp=@head
+  while (temp.next_node.next_node!=nil)
+    temp=temp.next_node
+  end
+  temp.next_node=nil
+  @tail=temp
+end
+
+def contains?(value)
+  temp=@head
+  value_found=false
+  while (temp!=nil)
+    if temp.data==value
+      value_found=true
+    end
+    temp=temp.next_node
+  end
+  value_found
+end
+
+def find(data)
+  temp=@head
+  node_index=0
+  while (temp.data!=data && temp!=nil)
+    temp=temp.next_node
+    node_index+=1
+  end
+  node_index
+end
+
+def to_s
+#format: ( data ) -> ( data ) -> ( data ) -> nil
+temp=@head
+str="(#{@head.data})->"
+while (temp.next_node!=nil)
+  temp=temp.next_node
+  str+="(#{temp.data})->"
+end
+  str+="nil"
+  puts str
+end
 
 class Node
-attr_accessor :next, :data
-def initialize(data=nil, nex=nil)
+attr_accessor :next_node, :data
+def initialize(data=nil, next_node=nil)
   @data=data
-  @next=nex  
-end
-
-def set_value(val)
-  @data=val
-end
-
-def next_node(nextnode)
-  @next=nextnode
-end
-
-def value
-  @data
-end
-def next
-  @next
+  @next_node=next_node 
 end
 
 end
 
-list=LinkedList.new 
-list.prepend("c")
-list.prepend("b")
+list=LinkedList.new
+list.append("a")
+list.append("b")
+list.append("c")
+list
